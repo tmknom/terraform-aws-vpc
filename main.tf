96fdea17e9a837b6edea77766157c58ef1850846
tmknom <tmk.nom@gmail.com>
2018-11-02 23:06:25 +0900

    Add instance_tenancy to VPC

--- main.tf
+++ main.tf
@@ -1,4 +1,6 @@
 # https://www.terraform.io/docs/providers/aws/r/vpc.html
 resource "aws_vpc" "default" {
   cidr_block = "${var.cidr_block}"
+
+  instance_tenancy     = "${var.instance_tenancy}"
 }
--- variables.tf
+++ variables.tf
@@ -2,3 +2,9 @@ variable "cidr_block" {
   type        = "string"
   description = "The CIDR block for the VPC."
 }
+
+variable "instance_tenancy" {
+  default     = "default"
+  type        = "string"
+  description = "A tenancy option for instances launched into the VPC."
+}
