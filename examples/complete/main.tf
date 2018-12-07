95a6d015c5d8690e6a3593ab4e74faac9324e869
tmknom <tmk.nom@gmail.com>
2018-12-07 16:02:21 +0900

    Tweak complete example

--- examples/complete/main.tf
+++ examples/complete/main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
   source     = "../../"
-  cidr_block = "192.168.0.0/16"
-  name       = "complete"
+  cidr_block = "10.255.0.0/16"
+  name       = "example"
 
-  public_subnet_cidr_blocks  = ["192.168.0.0/24", "192.168.1.0/24"]
+  public_subnet_cidr_blocks  = ["10.255.0.0/24", "10.255.1.0/24"]
   public_availability_zones  = ["ap-northeast-1a", "ap-northeast-1c"]
-  private_subnet_cidr_blocks = ["192.168.64.0/24", "192.168.65.0/24"]
+  private_subnet_cidr_blocks = ["10.255.64.0/24", "10.255.65.0/24"]
   private_availability_zones = ["ap-northeast-1a", "ap-northeast-1c"]
 
   instance_tenancy        = "default"
@@ -13,7 +13,7 @@ module "vpc" {
   enable_dns_hostnames    = false
   map_public_ip_on_launch = false
 
-  enabled_nat_gateway        = false
+  enabled_nat_gateway        = true
   enabled_single_nat_gateway = false
 
   tags = {
